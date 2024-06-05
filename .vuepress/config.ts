aade6a19973924abddb10cbd15df29cc4b251522
“QingFeng-awa” <qingfeng_awa@163.com>
2024-06-05 11:52:03 +0800

    2024.6.5

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -12,13 +12,15 @@ export default defineUserConfig({
   bundler: viteBundler(),
   theme: recoTheme({
     home: '/index.md',
+    primaryColor: '#5eccff',
+    catalogTitle: '目录',
     repo: 'XtremeWave/TownOfNewEpic_XtremeSite',
     docsRepo: 'https://github.com/XtremeWave/TownOfNewEpic_XtremeSite',
     docsBranch: 'main',
     style: "@vuepress-reco/style-default",
     logo: "https://cn-sy1.rains3.com/xtremewave/TONEX.jpg",
     author: "XtremeWave",
-    authorAvatar: "/head.png",
+    authorAvatar: "https://cn-sy1.rains3.com/xtremewave/XtremeWave.png",
     navbar: [
       {text: '首页', link: '/'},
       {text: '关于', link: '/about'},
@@ -35,42 +37,40 @@ export default defineUserConfig({
       {text: '疑难', link: '/FAQ'},
     ],
     series: {
-      '/Impostor/': [ '/Impostor/BountyHunter','/Impostor/Mare','/Impostor/Fireworker' ],
+      '/Impostor/': [ '/Impostor/BountyHunter','/Impostor/Mare','/Impostor/Fireworker','/Impostor/SerialKiller' ],
     },
-    //bulletin: {
-    //  body: [
-    //    {
-    //      type: "text",
-    //      content: `TONEX官方网站尚未完善职业文档。<br>
-    //      我们将尽快完成职业文档,感谢您的理解。`,
-    //      style: "font-size: 12px;",
-    //    },
-    //    {
-    //      type: "hr",
-    //    },
-    //    {
-    //      type: "buttongroup",
-    //      children: [
-    //        {
-    //          text: "赞助",
-    //          link: "https://www.xtreme.net.cn/donate",
-    //        },
-    //      ],
-    //    },
-    //  ],
-    //},
-    // commentConfig: {
-    //   type: 'valine',
-    //   // options 与 1.x 的 valineConfig 配置一致
-    //   options: {
-    //     // appId: 'xxx',
-    //     // appKey: 'xxx',
-    //     // placeholder: '填写邮箱可以收到回复提醒哦！',
-    //     // verify: true, // 验证码服务
-    //     // notify: true,
-    //     // recordIP: true,
-    //     // hideComments: true // 隐藏评论
-    //   },
-    // }, 
+    bulletin: {
+     body: [
+       {
+         type: "text",
+         content: `职业文档更新策略发生了变化。<br>如需了解详细信息,请<a href="https://github.com/XtremeWave/TownOfNewEpic_XtremeSite/issues/9" target="_blank" >点击此处。</a>`,
+         style: "font-size: 12px;",
+       },
+       {
+         type: "hr",
+       },
+       {
+         type: "buttongroup",
+         children: [
+           {
+             text: "赞助",
+             link: "https://www.xtreme.net.cn/donate",
+           },
+         ],
+       },
+     ],
+    },
+    //commentConfig: {
+    //  type: 'valine',
+    //  options: {
+    //    appId: 'xxx',
+    //    appKey: 'xxx',
+    //    placeholder: '填写邮箱可以收到回复提醒哦！',
+    //    verify: true,
+    //    notify: true,
+    //    recordIP: true,
+    //    hideComments: true
+    //  },
+    //}, 
   }),
 });
---
+++ series/Impostor/SerialKiller.md
@@ -0,0 +1,7 @@
+---
+permalink: /Impostor/SerialKiller.html
+title: 嗜血杀手
+description: 
+date: 2024-05-21
+---
+![嗜血杀手](https://cn-sy1.rains3.com/xtremewave/SerialKiller.png)
